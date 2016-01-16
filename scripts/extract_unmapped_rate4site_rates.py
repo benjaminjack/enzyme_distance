c9c407a15766cdd285715aff1537c0ecc95f1106
Benjamin Jack <benjamin.r.jack@gmail.com>
2016-01-15 18:09:33 -0600

    Updated rate4site parsing script.

--- scripts/extract_unmapped_rate4site_rates.py
+++ scripts/extract_unmapped_rate4site_rates.py
@@ -11,16 +11,16 @@ However, in this script that rates are not mapped back to the alignment
 def get_rates(rate_file):
 	'''
 	This is a function that extracts and maps the evolutionary rates to the pdb sequence
-	
+
 	Args:
 
-		map_file: The file that maps the pdb sequence to the alignment	
+		map_file: The file that maps the pdb sequence to the alignment
 		rate_file: The rate4site file
 	Returns:
-		sites: The sites in the pdb that have been mapped to the alignment	
-		aligned_rates: The rates mapped back to sites in the pdb	
+		sites: The sites in the pdb that have been mapped to the alignment
+		aligned_rates: The rates mapped back to sites in the pdb
 	'''
-			
+
 	align_sites = []
 	rates = []
 	amino_acids = []
@@ -28,18 +28,21 @@ def get_rates(rate_file):
 
 	input = open(rate_file)
 	data = input.readlines()
-	data = data[13:]
-	data.pop()
-	data.pop()
 
 	for line in data: #Get the evolutionary rates from the rate4site file
-		site = line[0:4].strip()
-		amino = line[5:10].strip()
+		if line[0] == "#" or line.strip() == "":
+			continue
+		line_list = line.split()
+		site = line_list[0]
+		amino = line_list[1]
+		# site = line[0:5].strip()
+		# amino = line[5:10].strip()
 		ref_seq_array.append(amino)
-		rate = line[10:19].strip()
+		# rate = line[10:19].strip()
+		rate = line_list[2]
 		align_sites.append(site)
 		amino_acids.append(amino)
-		rates.append(rate)	
+		rates.append(rate)
 
 	aligned_rates = []
 	counter = 0
@@ -49,13 +52,13 @@ def get_rates(rate_file):
 def extract_sites(map_file):
 	'''
 	This is a function extracts the site information from the mapped file
-	
+
 	Args:
 		pdb: The pdb name
 		map_file: The file that maps the pdb sequence to the alignment
 		rate_file: the rate4site file
 	Returns:
-		sites: In this case just the pdb_aa 		
+		sites: In this case just the pdb_aa
 	'''
 
 	site_data = np.genfromtxt(map_file, delimiter = "\t", dtype = "string", skip_header = 1, usecols = (0)) #Read in the data
@@ -67,11 +70,11 @@ def main():
 	if len( sys.argv ) != 3:
 		print '''      Usage:      '''
 		print "     ", sys.argv[0], "<input rate4site file>", "<output file>"
-								
+
 	else:
 		rate_file = sys.argv[1] #rate4site file
 		outfile = sys.argv[2] #outfile name
-		
+
 		out = open(outfile, "w")
 		out.write('"pdb_aa","rate"\n')
 
@@ -90,8 +93,8 @@ def main():
 			out.write(str(sites[j]) + ',' + str(rates[j]) + "\n") #Write out the rate information for that site
 			# print str(rates[j])
 			j = j + 1
-	
-		out.close()		
+
+		out.close()
 
 if __name__ == "__main__":
 	main()
